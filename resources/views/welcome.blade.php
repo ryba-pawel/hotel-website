44e5dabb2b9dc059ea37d506141de821346038cb
Pawel <p.ryba@inter-system.info>
2022-11-24 10:21:55 +0100

    Zmiany
    2022.11.24 - front end

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
           </svg>
         </div>
         <div class="flex-grow">
-          <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Shooting Stars</h2>
+          <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Shooting Stars@@@</h2>
           <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
           <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
@@ -40,7 +40,7 @@
           </svg>
         </div>
         <div class="flex-grow">
-          <h2 class="text-gray-900 text-lg title-font font-medium mb-3">The Catalyzer</h2>
+          <h2 class="text-gray-900 text-lg title-font font-medium mb-3">The Catalyzer$$$</h2>
           <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
           <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
